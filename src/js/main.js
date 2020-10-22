1e2eccaf3d4b4d1e2233d218ecc00f4a4b43a976
RusTaggs <udachin_2000@mail.ru>
2020-10-22 14:52:43 +0300

    desktop final

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,16 @@
-//alert("Hello");
+
+
+var mySwiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    
+    
+    speed: 400,
+    spaceBetween: 100,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    
+  })
