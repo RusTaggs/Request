beab3f3bfff371940d069d139ddf7eb7ef56b07d
RusTaggs <udachin_2000@mail.ru>
2020-10-27 17:53:24 +0300

    Final

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,39 @@
-
-
 var mySwiper = new Swiper('.swiper-container', {
     // Optional parameters
     
     
     speed: 400,
-    spaceBetween: 100,
+    spaceBetween: 50,
   
     // If we need pagination
     pagination: {
       el: '.swiper-pagination',
     },
+    navigation:{
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    }
   
     
-  })
+});
+
+$(document).ready(function(){
+  $(".toggle-bar").click(function(){
+    $('.main-menu').toggleClass('active');
+    $('body').toggleClass('active');
+  });
+  $(".header-equipment__mobile").click(function(){
+    $('.equipment-menu').addClass('active');
+    $('body').addClass('active');
+  });
+  $(".equipment__exit").click(function(){
+    $('.equipment-menu').removeClass('active');
+    $('body').removeClass('active');
+  });
+
+  $(".show-more-button").click(function(){
+    $('.portfolio-left__text3').slideToggle("slow");
+    $('.show-more-button').toggle()
+  });
+
+})
